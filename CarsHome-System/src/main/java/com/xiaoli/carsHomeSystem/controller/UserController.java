0fccc4f560a014d2d0991dab4252372e5da2874b
xiaolijiushishuai <836075337@qq.com>
2020-03-01 22:59:55 +0800

    1.1

--- CarsHome-System/src/main/java/com/xiaoli/carsHomeSystem/controller/UserController.java
+++ CarsHome-System/src/main/java/com/xiaoli/carsHomeSystem/controller/UserController.java
@@ -1,18 +1,36 @@
 package com.xiaoli.carsHomeSystem.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.xiaoli.carsHomeServiceApi.systemService.UserService;
+import com.xiaoli.carsHomeSystem.service.UserServiceImpl;
+import com.xiaoli.carshomemodel.System.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
     @GetMapping("/check/{id}")
     public String check(@PathVariable("id") String id){
         return "xiaolijiushizhemeshuai6";
     }
+
+    @GetMapping("/checkUser/{userName}")
+    public User checkUser(@PathVariable("userName") String userName){
+        return userServiceImpl.checUser(userName);
+    }
+    //this is a problem    404 error
+    /*@PostMapping("/model")
+    public ModelAndView getMesge(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test.html");
+        return modelAndView;
+    }*/
 }
+
