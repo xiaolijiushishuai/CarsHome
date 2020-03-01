e73b9ce825d6c69ed30057910a6cc4a7a09ef7d7
xiaolijiushishuai <836075337@qq.com>
2020-03-01 23:00:52 +0800

    1.1

---
+++ CarsHome-model/src/main/java/com/xiaoli/carshomemodel/System/domain/User.java
@@ -0,0 +1,45 @@
+package com.xiaoli.carshomemodel.System.domain;
+
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+    private String id;
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
