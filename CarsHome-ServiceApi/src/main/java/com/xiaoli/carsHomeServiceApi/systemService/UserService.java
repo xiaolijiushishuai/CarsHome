063f6b24d12245946691560af4f0329ef97b13d8
xiaolijiushishuai <836075337@qq.com>
2020-03-01 23:02:01 +0800

    1.1

---
+++ CarsHome-ServiceApi/src/main/java/com/xiaoli/carsHomeServiceApi/systemService/UserService.java
@@ -0,0 +1,8 @@
+package com.xiaoli.carsHomeServiceApi.systemService;
+
+
+import com.xiaoli.carshomemodel.System.domain.User;
+
+public interface UserService {
+    public User checUser(String userName);
+}
